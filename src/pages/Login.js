8ed9acaf9d7bd1bc49ea433d2d010cd27f580c6f
Tharindu De Costa <mtndecosta@gmail.com>
2024-10-17 11:09:20 +0530

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { axiosclient } from "../api";
+import Swal from "sweetalert2";
+import Cookies from 'js-cookie';
+import { jwtDecode } from "jwt-decode";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -8,12 +11,67 @@ const Login = () => {
 
   const [showPassword, setshowPassword] = useState(false);
 
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setLogin({
+      ...login,
+      [name]: value,
+    });
+  };
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate("/home");
-    // await login(email,password)
+  
+    axiosclient
+      .post("/api/User/login", login)
+      .then((res) => {
+        if (res && res.data) {
+          const token = res.data.token;
+          console.log("JWT Token:", token);
+          
+          if (token) {
+            const tokenData = {
+              jwtToken: token,
+            };
+            Cookies.set("token", JSON.stringify(tokenData), { expires: 1 }); 
+  
+            const decoded = jwtDecode(token);
+            const id = decoded.unique_name; 
+            Cookies.set("userId", JSON.stringify({ userId: id }), { expires: 1 });
+    
+            Swal.fire("LOGIN", "LOGIN_SUCCCESSFUL", "success").then(() => {
+              navigate("/home");
+            });
+          }
+        } else {
+          console.error('Response or response data is undefined');
+        }
+      })
+      .catch((err) => {
+        Swal.fire("LOGIN", err.response?.data?.response || "Login failed", "").then(() => {
+          console.log(err);
+        });
+      });
+  };
+  
+  const fetchUserDetails = (id) => {
+    axiosclient
+      .get(`/api/User/${id}`)
+      .then((response) => {
+        const user = response.data;
+        console.log("User Name:", user.name);
+      })
+      .catch((err) => {
+        console.error("Failed to fetch user details", err);
+      });
   };
 
   const togglePassword = () => {
@@ -25,15 +83,23 @@ const Login = () => {
       <h3>Log In</h3>
       <label>Email address:</label>
       <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
+        name="email"
+        type="text"
+        id="country"
+        class={`input`}
+        placeholder="Email"
+        value={login.email}
+        onChange={handleChange}
       />
       <label>Password:</label>
       <input
+        name="password"
         type={showPassword ? "text" : "password"}
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
+        id="country"
+        class={`input`}
+        placeholder="Password"
+        value={login.password}
+        onChange={handleChange}
       />
       <input className="chkBox" type="checkbox" onClick={togglePassword} /> Show
       Password
